bd9f61816f42841f26998348e8a3da0ad60aae9b
charlie morris <charliemorris.96@gmail.com>
2023-06-03 16:59:17 +0100

    controller action setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save!
+      redirect_to root
+    else
+      flash.now[:alert] = 'Reel could not be uploaded'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def post_params
+    params.require.(:post).permit(:user_id, :place_id, :place_rating)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
