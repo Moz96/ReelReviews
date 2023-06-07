287c820d562c9b135d0d146fdbb1efc5923efda6
Nick Hennell-Foley <nicholas@nicholashennellfoley.com>
2023-06-07 18:51:38 +0100

    User added to seed file and post assosiated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+user = User.create(
+  email: "user1@example.com",
+  password: "password"
+)
+
 place = Place.create(
   name: "Test name",
   address: "Test address",
@@ -16,9 +21,9 @@ place = Place.create(
 )
 
 post = Post.new(
-  user_id: 1,
+  user_id: user.id,
   place_id: place.id,
   place_rating: 5
 )
-post.reels.attach(io: File.open('app/assets/videos/testreel.mp4'), filename: 'testreel.mp4', content_type: 'video/mp4')
+post.reel.attach(io: File.open('app/assets/videos/testreel.mp4'), filename: 'testreel.mp4', content_type: 'video/mp4')
 post.save
