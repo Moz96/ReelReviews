ac685f431cf614a38201077442e1f768524a4263
Nick Hennell-Foley <nicholas@nicholashennellfoley.com>
2023-06-10 13:12:21 +0100

    Added more videos and also images for places

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,36 @@ user = User.create(
   email: "user1@example.com",
   password: "password"
 )
-puts "creating places"
-5.times do
+puts "creating 5 places"
+5.times do |i|
   place = Place.create(
-    name: "Test name",
-    address: "Test address",
-    description: "Test description",
-    category: "Test category",
+    name: "Place #{i}",
+    address: "Adress #{i}",
+    description: "Description #{i}",
+    category: "Test Category",
     url: "https://example.com",
     opening_hours: "10am - 5pm"
   )
-
-  post1 = Post.new(
-    user_id: user.id,
-    place_id: place.id,
-    place_rating: 5
+  puts "adding image to place"
+  place.image.attach(
+    io: File.open("app/assets/images/placeimage#{i}.jpg"),
+    filename: "placeimage#{i}.jpg",
+    content_type: 'image/jpeg'
   )
-  post1.video.attach(io: File.open('app/assets/videos/testreel1.mp4'), filename: 'testreel1.mp4', content_type: 'video/mp4')
-  post1.save
 
-  post2 = Post.new(
-    user_id: user.id,
-    place_id: place.id,
-    place_rating: 5
-  )
-  post2.video.attach(io: File.open('app/assets/videos/testreel2.mp4'), filename: 'testreel2.mp4', content_type: 'video/mp4')
-  post2.save
+  3.times do |j|
+    puts "creating #{j} of 3 posts"
+    post = Post.new(
+      user_id: user.id,
+      place_id: place.id,
+      place_rating: 5
+    )
+    puts "adding video to post"
+    post.video.attach(
+      io: File.open("app/assets/videos/place_#{i}_video#{j}.mp4"),
+      filename: "place_#{i}_video#{j}.mp4",
+      content_type: 'video/mp4'
+    )
+    post.save
+  end
 end
