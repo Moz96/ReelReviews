0b680a544b067ea376858332dfc9477a616c0c75
charlie morris <charliemorris.96@gmail.com>
2023-06-23 10:01:40 +0100

    general changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -33,6 +33,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(place_params)
+    @place.user = current_user
     if @place.save!
       redirect_to places_path
     else
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   has_many :posts
   has_many :favourites, dependent: :destroy
+  belongs_to :user
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
