1de239fb268a61ea6d000166620b255cb5e6fdc2
charlie morris <charliemorris.96@gmail.com>
2023-06-23 10:10:07 +0100

    changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,16 @@
 class CommentsController < ApplicationController
   def new
-    @post = Post.find(params[:post_id])
-    @comment = Comment.new(post: @post)
-
-    respond_to do |format|
-      format.html {
-        # Handle regular HTML request
-        render partial: 'comments/form', locals: { comment: @comment }
-      }
-      format.js {
-        # Handle AJAX request
-        render partial: 'comments/form', locals: { comment: @comment }
-      }
-    end
+    @comment = Comment.new
   end
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user = current_user
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to post_path(@post), notice: 'Comment created successfully.' }
-        format.js { render partial: 'comments/comment', locals: { comment: @comment } }
-      else
-        format.html { redirect_to post_path(@post), alert: 'Failed to create comment.' }
-        format.js { render partial: 'comments/error', status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment created successfully.'
+    else
+      redirect_to post_path(@post), alert: 'Failed to create comment.'
     end
   end
 
