4b8ce6e136ebd9431b6cb4f1eb9a49bc6db1e7ab
charlie morris <charliemorris.96@gmail.com>
2023-06-24 11:33:56 +0100

    fixed navbar

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :place
   has_many :comments
-  has_many :favourites
+  has_many :favourites, class_name: 'Favourite'
 
   validates :video_url, presence: true
   validates :video_public_id, presence: true
